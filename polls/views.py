191d6f8ccb302ededd22398965aeea760e6bb4ac
ThanadolU <dolthanadolu@gmail.com>
2022-09-05 22:09:18 +0700

    update choice in polls questions

--- polls/views.py
+++ polls/views.py
@@ -18,12 +18,14 @@ class IndexView(generic.ListView):
         """
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
+
 def showtime(request) -> HttpResponse:
     """Return the local time and date."""
     thaitime = timezone.localtime()
     msg = f"<p>The time is {thaitime}.</p>"
     # return the msg in an HTTP response
     return HttpResponse(msg)
+
 
 class DetailView(generic.DetailView):
     model = Question
