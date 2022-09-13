310aba8d3792c1a588ebde3e611f2b1c030ac094
ThanadolU <dolthanadolu@gmail.com>
2022-09-13 14:52:57 +0700

    Add docstring for views and models already.

--- polls/models.py
+++ polls/models.py
@@ -1,16 +1,19 @@
+"""This module contains the Question and Choice models."""
 import datetime
-
 from django.utils import timezone
 from django.db import models
 from django.contrib import admin
 
 # Create your models here.
 class Question(models.Model):
+    """Question model for creating questions."""
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     end_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
+        """Return readable string of each question."""
         return self.question_text
 
     @admin.display(
@@ -20,14 +23,17 @@ class Question(models.Model):
     )
 
     def was_published_recently(self):
+        """Return boolean whether it was published recently."""
         now = timezone.localtime()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def is_published(self):
+        """Return boolean when the question was published."""
         now = timezone.localtime()
         return now >= self.pub_date
 
     def can_vote(self):
+        """Return boolean when voting is allowed."""
         now = timezone.localtime()
         if self.end_date is None:
             return now >= self.pub_date
@@ -35,9 +41,12 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
+    """Choice model for creating choice."""
+
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
+        """Return readable string of each choice."""
         return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
+"""This module contains the views of each page of the application."""
 from django.http import HttpResponseRedirect
 from django.utils import timezone
 from django.urls import reverse
@@ -7,30 +8,33 @@ from django.shortcuts import render, get_object_or_404
 from .models import Question, Choice
 from django.contrib import messages
 
-# Create your views here.
+
 class IndexView(generic.ListView):
+    """Index page of application."""
+
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """
-        Return the last five published questions (not including those set to be
-        published in the future).
-        """
+        """Return the last five published questions (not including those set to be published in the future)."""
         return Question.objects.filter(pub_date__lte=timezone.localtime()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
+    """Detail page of application."""
+
     model = Question
     template_name = 'polls/detail.html'
 
     def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
+        """Excludes any questions that aren't published yet."""
         return Question.objects.filter(pub_date__lte=timezone.localtime())
 
     def get(self, request, pk):
+        """Return different pages depend on is_published and can_vote.
+        
+        Return index page if is_published or can_vote are true. If not return detail page.
+        """
         question = get_object_or_404(Question, pk=pk) 
         if not question.is_published():
             messages.error(request, 'This poll is not published.')
@@ -42,10 +46,13 @@ class DetailView(generic.DetailView):
 
 
 class ResultsView(generic.DetailView):
+    """Result page of application."""
+
     model = Question
     template_name = 'polls/results.html'
 
     def get(self, request, pk):
+        """Return result page if is_published method returns true. If not redirect to the index page."""
         question = get_object_or_404(Question, pk=pk)
         if question.is_published():
             return render(request, 'polls/results.html', {'question': question,})
@@ -54,6 +61,7 @@ class ResultsView(generic.DetailView):
 
 
 def vote(request, question_id):
+    """Add vote to selected choice of current question."""
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
