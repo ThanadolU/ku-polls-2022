5313137fef886595bb1c993a1017f44cd8f3c79d
ThanadolU <dolthanadolu@gmail.com>
2022-09-23 21:36:39 +0700

    clean up code with flake8

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,8 @@
+"""This module contains the admin page of the application."""
 from django.contrib import admin
 from .models import Choice, Question
 
-# Register your models here.
+
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
@@ -10,11 +11,17 @@ class ChoiceInline(admin.TabularInline):
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date', 'end_date'], 'classes': ['collapse']}),
+        ('Date information', {'fields': ['pub_date', 'end_date'],
+                              'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date', 'end_date', 'was_published_recently', 'can_vote')
+    list_display = ('question_text',
+                    'pub_date',
+                    'end_date',
+                    'was_published_recently',
+                    'can_vote')
     list_filter = ['pub_date', 'end_date']
     search_fields = ['question_text']
+
 
 admin.site.register(Question, QuestionAdmin)
